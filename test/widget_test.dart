4bdf6d50f2bfed041e265dce6eed4a820aea72bd
Justin Thoreson <thoresonjd@gmail.com>
2021-11-19 10:19:09 -0800

    Test page error solved

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:static_profile_page/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const StaticProfilePageApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
